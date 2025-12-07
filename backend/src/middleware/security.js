2ac3f40dc96e5a3374c149480254b24c820a4493
MrANSEO <mranseo930@gmail.com>
2025-12-07 10:15:47 +0000

    Add new CORS origin for paymentsaafront
    arrangement du lien du nouveau frontend 'https://paymentsaafront.onrender.com'

--- backend/src/middleware/security.js
+++ backend/src/middleware/security.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const corsOptions = {
   origin: [
     'https://frontendpay.onrender.com', // ✅ SANS SLASH
+    'https://paymentsaafront.onrender.com'
     'http://localhost:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
